d05f8270281cae0febd1c86be575e46e03029c7b
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2021-11-09 03:03:25 +0500

    add/edit/delete

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: false },
+  { id: "todo-0", name: "Eat", completed: true },
   { id: "todo-1", name: "Sleep", completed: false },
   { id: "todo-2", name: "Repeat", completed: false }
 ];
